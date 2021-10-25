9b3ae49f8eeeee331578e1673e975f0006dee169
Steven Xia <stevenxia@protonmail.com>
2021-10-24 23:48:43 -0700

    [FrisbeeToss] Add initial project script.

---
+++ Projects/main.py
@@ -0,0 +1,109 @@
+# Discord music player bot. Only works with one server and voice channel.
+# requires: colorlog, discord.py, yt_dlp
+
+import asyncio
+from urllib.parse import urlparse
+
+import colorlog
+import discord
+from discord.ext import commands
+# import youtube_dl
+import yt_dlp as youtube_dl
+
+from src._env import TOKEN
+
+MAX_FILE_SIZE = 32 * 1024 * 1024    # bytes
+
+# ``logging'' configurations --------------------------------------------------
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter(
+    "%(log_color)s[%(levelname)s] %(message)s",
+    log_colors={
+        "DEBUG": "light_black",
+        "INFO": "white",
+        "WARNING": "yellow",
+        "ERROR": "red",
+        "CRITICAL": "bold_red",
+    }
+))
+
+logger = colorlog.getLogger()
+logger.addHandler(handler)
+logger.setLevel(colorlog.DEBUG)
+
+
+activity = discord.Activity(type=discord.ActivityType.listening, name="the abyss' howling.")
+bot = commands.Bot(command_prefix="!", case_insensitive=True, activity=activity)
+
+
+@bot.event
+async def on_ready() -> None:
+    logger.info(f"{bot.user.name} has connected to Discord!")
+
+
+@bot.command()
+async def play(ctx, *args):
+    voice = ctx.author.voice
+    if voice is None:
+        return await ctx.send(f"{ctx.author.mention} is not in a channel.")
+    voice_channel = voice.channel
+
+    # Set up the embed message.
+    embed = discord.Embed(title=surround_message("Processing request", ":arrows_counterclockwise:"))
+    message = await ctx.send(embed=embed)
+
+    # Process the arguments.
+    url = args[0] if urlparse(args[0]).scheme else "ytsearch1:" + " ".join(args)
+
+    # Preprocess the url.
+    # todo: look into option hls-use-mpegts
+    ytdl_opts = {
+        "format": "bestaudio",
+        "audio_format": "m4a",
+        "audio_quality": 0,
+        "outtmpl": "download.m4a",
+        "overwrites": True,
+    }
+    with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
+        info = ytdl.extract_info(url, download=False)
+        if "entries" in info:
+            info = info["entries"][0]
+
+        embed.title = surround_message("Downloading item", ":arrow_double_down:")
+        embed.add_field(name="Title", value=info["title"], inline=True)
+        embed.add_field(name="Uploader", value=info["uploader"], inline=True)
+        embed.set_image(url=info["thumbnail"])
+        embed.set_footer(text="Retrieved from: " + info["webpage_url"])
+        await message.edit(embed=embed)
+
+        if info["filesize"] > MAX_FILE_SIZE:
+            embed.title = surround_message("Sorry; item too large.", ":no_entry_sign:")
+            return await message.edit(embed=embed)
+
+        ytdl.download([info["webpage_url"]])
+
+    # Join the voice channel and play the item.
+    embed.title = surround_message("Now playing", ":musical_note:")
+    await message.edit(embed=embed)
+
+    vc = await voice_channel.connect()
+    vc.play(discord.FFmpegPCMAudio("download.m4a", options="-maxrate 96k -bufsize 192k"))
+
+    # Sleep while audio is playing.
+    while vc.is_playing():
+        await asyncio.sleep(0)
+
+    embed.title = surround_message("Finished playing", ":white_check_mark:")
+    await message.edit(embed=embed)
+
+    await vc.disconnect()
+
+
+# Utility functions.
+def surround_message(msg: str, surr: str, times: int = 1) -> str:
+    return f"{surr*times}  {msg}  {surr*times}"
+
+
+if __name__ == "__main__":
+    bot.run(TOKEN)
