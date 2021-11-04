d38d5c11156554270b026d51767cd0784e9b3f89
Steven Xia <33334736+xtevenx@users.noreply.github.com>
2021-11-04 15:22:51 -0700

    Add a basic template file.

---
+++ Computer Science/template.cc
@@ -0,0 +1,33 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("fast-math")
+
+#pragma GCC target("avx2")
+//#pragma GCC target("sse4.2")
+//#pragma GCC target("mmx")
+
+#pragma GCC target("popcnt,lzcnt")
+#define popcnt __builtin_popcount
+#define lzcnt __builtin_clz
+
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+
+int abs(int n) { return n >= 0 ? n : -n; }
+bool ascending(int n, int m) { return n < m; }
+bool descending(int n, int m) { return n > m; }
+
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+
+
+    return 0;
+}
