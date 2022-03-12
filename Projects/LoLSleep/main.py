e43b5136f98fe35c0c027ad1f73b97f3a70f2ef0
Steven Xia <stevenxia@protonmail.com>
2022-03-12 13:35:50 -0800

    [LoLSleep] Add more precision for time interval selection.

--- Projects/LoLSleep/main.py
+++ Projects/LoLSleep/main.py
@@ -3,16 +3,19 @@ import lcu_driver
 import psutil
 
 # Allowed start/end time windows by hour of the day.
-START_HOUR: int = 5
-END_HOUR: int = 23
+START_TIME = {"hour": 5, "minute": 0, "second": 0}
+END_TIME = {"hour": 10, "minute": 30, "second": 0}
 
 connector = lcu_driver.Connector()
 
 
 @connector.ws.register("/lol-chat/v1/me")
 async def status_update(connection, event):
+    now = datetime.datetime.now()
+    start = now.replace(**START_TIME)
+    end = now.replace(**END_TIME)
     if (await (await connection.request("get", "/lol-chat/v1/me")).json()).get("availability") != "dnd" \
-            and not START_HOUR <= datetime.datetime.now().hour < END_HOUR:
+            and not start <= now < end:
         # connector.ws.registered_uris.clear()
         for p in psutil.process_iter():
             if p.name() == "LeagueClient.exe":
