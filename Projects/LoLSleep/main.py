a2a7c796927f8bc79c185ad054c6b913db2fe03a
Steven Xia <stevenxia@protonmail.com>
2022-03-12 14:39:36 -0800

    [LoLSleep] Reduce CPU usage when idle.

--- Projects/LoLSleep/main.py
+++ Projects/LoLSleep/main.py
@@ -1,12 +1,17 @@
 import datetime
-import lcu_driver
+import lcu_driver.utils
 import psutil
 
-# Allowed start/end time windows by hour of the day.
 START_TIME = {"hour": 5, "minute": 0, "second": 0}
-END_TIME = {"hour": 10, "minute": 30, "second": 0}
+END_TIME = {"hour": 22, "minute": 30, "second": 0}
 
 connector = lcu_driver.Connector()
+
+
+# Hack the library to burn less CPU when League is not running.
+from time import sleep
+_foo = lcu_driver.utils.return_process
+lcu_driver.utils.return_process = lambda *a, **k: [sleep(5), _foo(*a, **k)][1]
 
 
 @connector.ws.register("/lol-chat/v1/me")
@@ -14,8 +19,11 @@ async def status_update(connection, event):
     now = datetime.datetime.now()
     start = now.replace(**START_TIME)
     end = now.replace(**END_TIME)
-    if (await (await connection.request("get", "/lol-chat/v1/me")).json()).get("availability") != "dnd" \
-            and not start <= now < end:
+
+    if not start <= now < end \
+            and (await (await connection.request(
+        "get", "/lol-chat/v1/me"
+    )).json()).get("availability") != "dnd":
         # connector.ws.registered_uris.clear()
         for p in psutil.process_iter():
             if p.name() == "LeagueClient.exe":
